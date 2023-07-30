a8398c01aba3d774a94434e6d461f7ed7e1f76ff
Ankezz <107862394+Ankezz@users.noreply.github.com>
2023-07-30 16:24:05 +0400

    Add files via upload

---
+++ Test/zadanie 1 и 3.php
@@ -0,0 +1,89 @@
+<?php
+// Задание 1 
+$categories = array(
+    array(
+        "id" => 1,
+        "title" => "Обувь",
+        'children' => array(
+            array(
+                'id' => 2,
+                'title' => 'Ботинки',
+                'children' => array(
+                    array('id' => 3, 'title' => 'Кожа'),
+                    array('id' => 4, 'title' => 'Текстиль'),
+                ),
+            ),
+            array(
+                'id' => 5,
+                'title' => 'Кроссовки',
+            ),
+            
+        )
+    ),
+    array(
+        "id" => 6,
+        "title" => "Спорт",
+        'children' => array(
+            array(
+                'id' => 7,
+                'title' => 'Мячи'
+            )
+        )
+    ),
+);
+
+function searchCategory($categories, $id)
+{
+    foreach ($categories as $category) {
+        if ($category['id'] === $id) {
+            return $category['title'];
+        }
+
+        if (isset($category['children'])) {
+            $result = searchCategory($category['children'], $id);
+            if ($result !== null) {
+                return $result;
+            }
+        }
+    }
+
+    return null; // не найдено
+}
+
+echo ("<h3>Задание 1 : </h1>");
+echo searchCategory($categories, 4); // Выведет: по id текстиль
+echo searchCategory($categories, 7); // Выведет: по id Мячи
+echo ("</br>");
+echo ("<h3>Задание 3 : </h1>");
+//Задание 3 
+function isCorrectHTMLStructure($tagsArray)
+{
+    $stack = array();
+
+    foreach ($tagsArray as $tag) {
+
+        if (strpos($tag, '</') === false) {
+            // Добавляем  в массив
+            array_push($stack, $tag);
+        } else {
+            // проверка с последним
+            $lastTag = array_pop($stack);
+
+            // если не соответсвует
+            if ($lastTag === null || $lastTag !== substr($tag, 1, -1)) {
+                return false;
+            }
+        }
+    }
+
+    // если сооттвествует
+    return empty($stack);
+}
+
+// Пример использования:
+$tagsSequence1 = ["<a>", "<div>", "</div>", "</a>", "<span>", "</span>"];
+$tagsSequence2 = ["<a>", "<div>", "</a>"];
+
+var_dump(isCorrectHTMLStructure($tagsSequence1)); // Выведет: bool(true)
+var_dump(isCorrectHTMLStructure($tagsSequence2)); // Выведет: bool(false)
+?>
---
+++ Test/zadanie 2 и 4 .php
@@ -0,0 +1,66 @@
+<!-- ЗАДАНИЕ 2-->
+<!-- SELECT
+    worker.first_name,
+    worker.last_name,
+    (
+        SELECT GROUP_CONCAT(child.name SEPARATOR ', ')
+        FROM child
+        WHERE child.user_id = worker.id
+    ) AS children,
+    (
+        SELECT MAX(car.model)
+        FROM car
+        WHERE car.user_id = worker.id
+    ) AS model
+FROM
+    worker;
+    // GROUP_CONCAT - превращение в одну строку
+    // MAX(car.model) чтоб выводило одно значение посстоянно  -->
+    <?php
+
+//*Задание 4
+interface BoxInterface {
+    public function setData($key, $value);
+    public function getData($key);
+    public function save();
+    public function load();
+}
+
+// Класс-оболочка FileBox
+class FileBox implements BoxInterface {
+    private $data = [];
+// название файла
+    private $file = 'data.txt';
+
+    public function setData($key, $value) {
+        $this->data[$key] = $value;
+    }
+
+    public function getData($key) {
+        return $this->data[$key] ?? null;
+    }
+// сохранение в виде file_put - запись в файл,serialize преоброзование в строку
+    public function save() {
+        file_put_contents($this->file, serialize($this->data));
+    }
+// unserialize - возвращает обратно
+    public function load() {
+        if (file_exists($this->file)) {
+            $data = file_get_contents($this->file);
+            $this->data = unserialize($data);
+        }
+    }
+}
+
+// пример
+$fileBox = new FileBox();
+$fileBox->setData('name', 'Андрей Константинов');
+$fileBox->setData('age', 20);
+$fileBox->save();
+
+$loadedBox = new FileBox();
+$loadedBox->load();
+echo $loadedBox->getData('name');
+echo $loadedBox->getData('age');  
+
+ 
